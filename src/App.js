0fe7dd5a6f8b241106d02826907755223825c255
osmarpetry <osmarpetry@gmail.com>
2018-11-08 01:39:02 -0200

    simple mock with a table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import mock from './mock/hand-points.js'
 
-function makeApointment() {
-  console.log('makeApointment()')
-}
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 
 class App extends Component {
+
+  handPoint() {
+    console.log('handPoint()')
+  }
+
   render() {
     return (
-      <div className="App">
-        <button onClick={ () => makeApointment() }>
+      <AppContainer>
+        <button onClick={ () => this.handPoint() }>
           Fazer apontamento
         </button>
-      </div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Horário</th>
+            </tr>
+          </thead>
+          <tbody>
+          {
+            mock.map((val, index) =>
+              <tr key={index}>
+                <td>{ val.id }</td>
+                <td>{ val.date }</td>
+              </tr>
+            )
+          }
+          </tbody>
+        </table>
+      </AppContainer>
     );
   }
 }
