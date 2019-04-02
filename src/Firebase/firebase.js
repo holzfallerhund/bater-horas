cb3ccbbdc30b22b434f4cdd30deb00f5ba599a44
Osmar Petry <osmarpetry@gmail.com>
2019-04-02 12:09:37 -0300

    firebase auth methods

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const prodConfig = {
     apiKey: process.env.REACT_APP_PROD_API_KEY,
@@ -24,7 +25,22 @@ const config =
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
