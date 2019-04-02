9024ccdbc149e268cb599fae0819bbf8e8b6ec59
Osmar Petry <osmarpetry@gmail.com>
2019-04-02 00:00:17 -0300

    navbar botton is working, not that whell but works

--- src/NavbarBotton/NavbarBotton.js
+++ src/NavbarBotton/NavbarBotton.js
@@ -3,21 +3,42 @@ import React, { PureComponent } from 'react'
 
 export default class NavbarBotton extends PureComponent {
     state = {
-        isActive: false
+        isActive: false,
+        isActiveSelect: false
     }
 
     render() {
         const isActive = this.state.isActive && ' is-active'
+        const isActiveSelect = this.state.isActiveSelect && ' is-active'
+
         return (
             <nav className='navbar is-light is-fixed-bottom' role='navigation' aria-label='main navigation'>
                 <div className='navbar-brand'>
+                    <a
+                        href='/#'
+                        role='button'
+                        className={`navbar-burger burger ${isActive}`}
+                        aria-label='menu'
+                        aria-expanded='false'
+                        data-target='botton-navbar'
+                        onClick={() => {
+                            this.setState({
+                                isActive: !this.state.isActive
+                            })
+                        }}>
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                        <span aria-hidden='true'></span>
+                    </a>
+                </div>
+                <div id='botton-navbar' className={`navbar-menu ${isActive}`}>
                     <div className='navbar-start'>
-                        <div className={'navbar-item is-hidden-touch has-dropdown has-dropdown-up ' + isActive} >
+                        <div className={'navbar-item has-dropdown has-dropdown-up ' + isActiveSelect} >
                             <a
                                 className='navbar-link'
                                 onClick={() => {
                                     this.setState({
-                                        isActive: !this.state.isActive
+                                        isActiveSelect: !this.state.isActiveSelect
                                     })
                                 }}>
                                 2019
@@ -25,6 +46,10 @@ export default class NavbarBotton extends PureComponent {
                             <div className='navbar-dropdown'>
                                 <a href='/' className='navbar-item'>
                                     2019
+                                </a>
+                                <hr class="navbar-divider" />
+                                <a href='/' className='navbar-item'>
+                                    Adicionar
                                 </a>
                             </div>
                         </div>
