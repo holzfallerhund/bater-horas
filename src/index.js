583fa3800848c3efca2c07d1ae5d5919c2dd4673
osmarpetry <osmarpetry@gmail.com>
2019-06-16 17:36:11 -0300

    manifest json

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import Firebase, { FirebaseContext } from './components/firebase'
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-            console.log('SW registered: ', registration)
-        }).catch(registrationError => {
-            console.log('SW registration failed: ', registrationError)
-        })
-    })
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => console.log('service worker registered'))
+        .catch(err => console.log('service worker not registered', err))
 }
 
 ReactDOM.render(
